1508ee86349acd60490c73861f17ad9ad039e319
MuriloWeyne <murilopw@al.insper.edu.br>
2023-06-16 03:21:52 -0300

    Challengeeeeeeeeer

--- modules/leaguefunctions.py
+++ modules/leaguefunctions.py
@@ -1,7 +1,14 @@
 import os
 import requests
+import time
+import json
 
 from base64 import b64encode
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+
+session = requests.Session()
 
 
 def get_lock_file():
@@ -11,7 +18,7 @@ def get_lock_file():
         with open(path_file, encoding="UTF-8") as lock_file:
                 lock_file_data = lock_file.read().split(":")
                 return lock_file_data
-    
+        
 def encrypt_headers():
     password = get_lock_file()[3]
     base64 = b64encode(bytes(f"riot:{password}", "UTF-8"))
@@ -26,21 +33,24 @@ def get_summoner_id():
     summoner_ep = "lol-summoner/v1/current-summoner"
     headers = encrypt_headers()
     url = get_url(summoner_ep)
-    response = requests.get(url, verify=False, headers=headers).json()
+    session.mount(url, HTTPAdapter(max_retries=Retry(connect=30, backoff_factor=0.5, status_forcelist=[404, 500, 502, 503, 504])))
+    response = session.get(url, verify=False, headers=headers)
     return response["summonerId"]
 
 def get_summoner_puuid():
     summoner_ep = "lol-summoner/v1/current-summoner"
     headers = encrypt_headers()
     url = get_url(summoner_ep)
-    response = requests.get(url, verify=False, headers=headers).json()
+    session.mount(url, HTTPAdapter(max_retries=Retry(connect=30, backoff_factor=0.5, status_forcelist=[404, 500, 502, 503, 504])))
+    response = session.get(url, verify=False, headers=headers)
     return response["puuid"]
 
 def get_summoner_data():
     summoner_ep = "lol-summoner/v1/current-summoner"
     headers = encrypt_headers()
     url = get_url(summoner_ep)
-    response = requests.get(url, verify=False, headers=headers).json()
+    session.mount(url, HTTPAdapter(max_retries=Retry(connect=30, backoff_factor=0.5, status_forcelist=[404, 500, 502, 503, 504])))
+    response = session.get(url, verify=False, headers=headers)
     summoner_name = response["displayName"]
     summoner_level = response["summonerLevel"]
     return summoner_name, summoner_level
@@ -50,7 +60,9 @@ def get_ranked_info():
     ranked_ep = f"lol-ranked/v1/ranked-stats/{summoner_puuid}"
     headers = encrypt_headers()
     url = get_url(ranked_ep)
-    response = requests.get(url, verify=False, headers=headers).json()       
+    session.mount(url, HTTPAdapter(max_retries=Retry(connect=30, backoff_factor=0.5, status_forcelist=[404, 500, 502, 503, 504])))
+    response = session.get(url, verify=False, headers=headers)
+    
     ranked_stats = response["queueMap"]["RANKED_SOLO_5x5"]
     summoner_rank = ranked_stats["tier"] + " " + ranked_stats["division"]
     summoner_lp = ranked_stats["leaguePoints"]
@@ -60,11 +72,23 @@ def get_ranked_info():
     return summoner_rank, summoner_lp, summoner_wins, summoner_losses, summoner_winrate
 
 def get_currencies():
-    currencies_ep = "lol-store/v1/wallet"
+    currency_ep = "lol-store/v1/wallet"
     headers = encrypt_headers()
-    url = get_url(currencies_ep)
-    response = requests.get(url, verify=False, headers=headers).json()
+    url = get_url(currency_ep)
+    session.mount(url, HTTPAdapter(max_retries=Retry(connect=30, backoff_factor=0.5, status_forcelist=[404, 500, 502, 503, 504])))
+    response = session.get(url, verify=False, headers=headers)
     be_amount = response["ip"]
+    rp_amount = response["rp"]
     if be_amount >= 1000:
         be_amount = str(round(be_amount/1000, 2)) + "K"
-    return response["rp"], be_amount
+    if rp_amount >= 1000:
+        rp_amount = str(round(rp_amount/1000, 2)) + "K"
+    return be_amount, rp_amount
+
+def is_signed_in():
+    login_ep = "lol-login/v1/session"
+    headers = encrypt_headers()
+    url = get_url(login_ep)
+    session.mount(url, HTTPAdapter(max_retries=Retry(connect=30, backoff_factor=0.5, status_forcelist=[404, 500, 502, 503, 504])))
+    response = session.get(url, verify=False, headers=headers)
+    return response.status_code
